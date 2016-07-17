d12238ca8b4832b4ab55b2cbaa98f8a17da5b489
Unai <unai.shin@gmail.com>
2016-07-17 12:34:59 +0200

    Primeros scripts y links

---
+++ Unity 1_Bases/Assets/Scripts/ScriptMorir.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Colisiones : MonoBehaviour 
+{
+	void OnCollisionEnter (Collision coli)
+	{
+		cambiarNivel ();
+	}
+
+	void cambiarNivel ()
+	{
+		SceneManager.LoadScene ("escena2");
+	}
+}
